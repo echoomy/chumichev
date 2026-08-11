9af847e6258b58f6f233a0e12aadbcb968c5cf76
Egor Chumichev <egor@chumichev.dev>
2026-08-11 12:38:51 +0500

    feat: get certificates for Passbolt and Samba services

--- services/reverse-proxy/images/resources/manager.sh
+++ services/reverse-proxy/images/resources/manager.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 while true; do
 	echo "Starting DNS-01 Challenge..."
-	./lego run --path /var/lib/lego/ --server $ACME_PROVIDER_NAME --dns yandexcloud --email $ACME_DOMAIN_EMAIL --domains $ACME_DOMAIN_NAME --domains code.$ACME_DOMAIN_NAME --dns.resolvers 8.8.8.8:53 --accept-tos
+	./lego run --path /var/lib/lego/ --server $ACME_PROVIDER_NAME --dns yandexcloud --email $ACME_DOMAIN_EMAIL --domains $ACME_DOMAIN_NAME --domains code.$ACME_DOMAIN_NAME --domains safe.$ACME_DOMAIN_NAME --domains file.$ACME_DOMAIN_NAME --dns.resolvers 8.8.8.8:53 --accept-tos
 
 	echo "Sleeping 24 hours..."
 	sleep 24h
